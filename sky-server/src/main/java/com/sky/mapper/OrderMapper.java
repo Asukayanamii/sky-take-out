a886b439e66dd230d9dfe1af573e822628422e74
Asukayanami <3184311912@qq.com>
2026-03-04 17:40:49 +0800

    订单状态定时处理

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -8,6 +8,9 @@ import com.sky.vo.OrderStatisticsVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Mapper
 public interface OrderMapper {
     void insert(Orders orders);
@@ -33,4 +36,7 @@ public interface OrderMapper {
     Page<Orders> pageSearch(OrdersPageQueryDTO ordersPageQuery);
 
     OrderStatisticsVO statistics();
+
+    @Select("select * from orders where status = #{status} and order_time < #{time}")
+    List<Orders> getByStatusAndOrderTime(Integer pendingPayment, LocalDateTime time);
 }
