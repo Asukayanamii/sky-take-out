f0b971d1fcd827fc6e9b8e996548028bdfad682e
Asukayanami <3184311912@qq.com>
2026-03-19 15:53:28 +0800

    工作台

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -10,6 +10,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -37,4 +38,10 @@ public interface DishMapper {
 
     @Select("select * from dish where status = 1 and category_id = #{categoryId}")
     List<Dish> getByCategoryId(Long categoryId);
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
